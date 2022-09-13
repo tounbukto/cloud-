fbc94ce074157efbbf32304a9ffa9fce28d50b5c
taha <kherraftaha@gmail.com>
2022-09-13 16:01:57 +0200

    with docker

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-import express from 'express'
+import express from "express"
+import postgres from "pg-promise"
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-let app = express();
+const app = express()
+const pg = postgres()
+const db = pg(process.env.DATABASE_URL || {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT || 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+});
+db.query('drop table if exists connexion; create table connexion(id int primary key, counter int default 0); insert into connexion values(0, 0)')
+console.log('db initialized')
 
 app.get('/', (req, res) => {
-    res.send('hello world')
-})
+  db.query("select counter from connexion")
+      .then((data) => {
+          let counter = data[0].counter || 0;
+          counter = counter + 1;
+          db.query(`update connexion set counter=${counter} where id=0`).catch((error) => console.log('ERROR', error));
+          res.send(`Hello World! x${counter}`);
+      })
+      .catch((error) => {
+          console.log("ERROR", error);
+      })
+});
 
-app.listen(PORT, () => console.log(`server start at port : ${PORT}`));
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
---
+++ init.sql
@@ -0,0 +1,3 @@
+drop table if exists;
+create table td1(id int, compteur int);
+insert into td1 values(0,0);
